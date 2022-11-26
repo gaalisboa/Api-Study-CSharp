fed58b26cf95cf2c40fc45ebffd56febbbd3c3d2
Gabriel de Paula Souza <gaahlisboa@gmail.com>
2022-11-26 19:15:56 -0300

    Routes created

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -1,15 +1,63 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPUdemy.src.Models;
+using RestWithASPUdemy.src.Services;
 
 namespace RestWithASPUdemy.src.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class PersonController
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
     {
+        private readonly IPersonService _personService;
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
 
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_personService.GetAll());
+        }
+
+        [HttpGet("getById/{id}")]
+        public IActionResult GetById(long id)
+        {
+            var person = _personService.GetById(id);
+            if (person == null)
+                return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpPost("createPerson")]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut("updatePerson")]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("deletePerson/{id}")]
+        public IActionResult Delete(long id)
+        {
+            var person = _personService.GetById(id);
+            if (person == null)
+                return NotFound();
+
+            _personService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
